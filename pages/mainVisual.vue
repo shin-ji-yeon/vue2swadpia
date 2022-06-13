5f8c6223b24a6e0e7c7f8d0497b328b7f4b85544
yong <ubbiyong@naver.com>
2022-06-13 16:54:37 +0900

    fullpage 추가, footer 위치변경, 비주얼영역 추가 퍼블, 틀어지는 css 조정

--- pages/mainVisual.vue
+++ pages/mainVisual.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="visual__swiper">
-    <!-- swiper1 -->
+  <div class="visual">
+    <!-- s: swiper img -->
     <swiper class="swiper swiper__visual-img" :options="swiperOptionImg" ref="swiperImg">
       <swiper-slide class="slide-1"><img src="../assets/images/main/main_bgimg01.jpg" alt=""></swiper-slide>
       <swiper-slide class="slide-2"><img src="../assets/images/main/main_bgimg01.jpg" alt=""></swiper-slide>
@@ -8,8 +8,9 @@
       <swiper-slide class="slide-4"><img src="../assets/images/main/main_bgimg01.jpg" alt=""></swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
+    <!-- e: swiper img -->
     <div class="visual__line">
-      <!-- swiper2 Thumbs -->
+      <!-- s: swiper2 text -->
       <swiper class="swiper swiper__visual-text" :options="swiperOptionText" ref="swiperText">
         <swiper-slide class="slide-1">
           <a href="" class="btn"><span class="blind">자세히보기</span></a>
@@ -48,6 +49,39 @@
           </div>
         </swiper-slide>
       </swiper>
+      <!-- e: swiper2 text -->
+      <!-- s: login -->
+      <div class="section__login">
+        <form name="" id="" method="" action="">
+            <div class="login__idpw">
+                <div class="input__row">
+                  <div class="inp-ico ico-id"><span class="blind">아이디</span></div>
+                  <input type="text" name="" id="" maxlength="40" class="inp-text" title="아이디" placeholder="아이디" />
+                </div>
+                <div class="input__row">
+                  <div class="inp-ico ico-pw"><span class="blind">비밀번호</span></div>
+                  <input type="password" name="" id="" maxlength="30" class="inp-text" title="비밀번호" placeholder="비밀번호" />
+                </div>
+            </div>
+            <div class="login__remember">
+              <input type="checkbox" name="user_saveid" id="user_saveid" class="inp-save" value="off" />
+              <label for="user_saveid" class="save-text">아이디 저장</label>
+            </div>
+            <div class="login__btn">
+                <button type="submit">로그인</button>
+            </div>
+            <ul class="login__find">
+              <li><a href="#">아이디 찾기</a></li>
+              <li><a href="#">비밀번호 찾기</a></li>
+              <li><a href="#">회원가입</a></li>
+            </ul>
+        </form>
+      </div>
+      <!-- e: login -->
+    </div>
+    <div class="visual__scroll">
+      <div class="text">SCROLL</div>
+      <div class="ico"></div>
     </div>
   </div>
 </template>
--- src/components/appFooter/appFooter.vue
+++ src/components/appFooter/appFooter.vue
@@ -1,18 +1,61 @@
 <template>
-  <header>footer
-    <p>style</p></header>
+  <footer class="footer">
+    <div class="footer__content">
+      <div class="footer__content-copyright">
+        <p>
+          <span>(주)성원애드피아  대표이사 정대원</span>
+          <span>서울특별시 성동구 성수일로 80</span>
+          <span>1599-5555</span>
+        </p>
+        <p>
+          <span>사업자등록번호 : 201-81-22977 <a href="#">사업자정보확인</a></span>
+          <span>호스팅제공자 : (주)성원애드피아</span>
+          <span>통신판매업신고 : 중구06853호</span>
+        </p>
+        <p>본 사이트의 모든 내용은 저작권법의 보호를 받으므로 무단 복제 및 배포를 금지합니다.</p>
+        <p>SUNGWONADPIA corporation. all rights reserved.</p>
+      </div>
+      <div class="footer__content-link">
+        <ul class="content-link__nav">
+          <li><a href="">이용약관</a></li>
+          <li><a href="">고객센터</a></li>
+          <li><a href="">개인정보처리방침</a></li>
+        </ul>
+        <div class="content-link__sns">
+          <a href="" class="sns__btn"><span class="blind">성원애드피아 sns</span></a>
+          <div class="sns__list">
+            <ul>
+              <li><a href="#" target="_blank"><span class="blind">인스타그램</span></a></li>
+              <li><a href="#" target="_blank"><span class="blind">유튜브</span></a></li>
+              <li><a href="#" target="_blank"><span class="blind">블로그</span></a></li>
+              <li><a href="#" target="_blank"><span class="blind">페이스북</span></a></li>
+              <li><a href="#" target="_blank"><span class="blind">카카오스토리</span></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="content-link__familysite">
+          <a href="" class="familysite__btn">FAMILY SITE</a>
+          <div class="familysite__list">
+            <ul>
+              <li><a href="#" target="_blank">성원 애드피아몰</a></li>
+              <li><a href="#" target="_blank">성원 푸드몰</a></li>
+              <li><a href="#" target="_blank">성원 로지스</a></li>
+              <li><a href="#" target="_blank">성원 애드피아</a></li>
+              <li><a href="#" target="_blank">스테이락 호텔</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 
 </script>
-
+<style>
+@import "./style.css";
+</style>
 <style lang="scss" scoped>
-header {
-  height: 100px;
-  background: #ddd;
-  p {
-    color: blue;
-  }
-}
+
 </style>
