90f2586dce43a49c00a0c4bcba55f85360d47d90
yong <ubbiyong@naver.com>
2022-06-14 14:41:53 +0900

    footer, main popup 추가/수정

--- src/index.vue
+++ src/index.vue
@@ -3,6 +3,7 @@
   <div id="app">
     <app-header/>
     <main-index/>
+    <main-popup/>
   </div>
 
 </template>
@@ -11,12 +12,14 @@
 
 import appHeader from '@/components/appHeader/appHeader.vue'
 import mainIndex from '/pages/mainIndex.vue'
+import mainPopup from '/pages/mainPopup.vue'
 
 export default {
   name: 'App',
   components: {
     appHeader,
-    mainIndex
+    mainIndex,
+    mainPopup
   }
 }
 </script>
